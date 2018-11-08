e216c8dc62d946027bcdd67678665bc88327d5d9
Administrator <xulei20084047@163.com>
2018-11-08 20:56:27 +0800

    test

---
+++ com/test/hello.py
@@ -0,0 +1,26 @@
+'''
+Created on 2018��11��6��
+
+@author: Administrator
+'''
+import sys;
+from pip._vendor.distlib.compat import raw_input
+
+'''
+注释
+'''
+# coding=gbk
+print("Hello World")
+if True:
+    print("This is true")
+else:
+    print("This is false")
+    
+raw_input("这是个阻断，按下enter键退出，按下其他键显示...\n");
+
+x = "This is test";
+print("x = " + x);
+
+x = "a"
+y = "b"
+print(x,y);
